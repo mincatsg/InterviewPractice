deee5fa3d6c8702135f24275e633486893e47617
mincatsg <1374723981@qq.com>
2020-01-21 12:03:36 +0800

    hoilday

---
+++ test16.java
@@ -0,0 +1,14 @@
+public class test16 {
+    //寻找数组的中心索引
+    public int pivotIndex(int[] nums) {
+        int sum = 0, leftsum = 0;
+        for (int x: nums){
+            sum += x;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (leftsum == sum - leftsum - nums[i]) return i;
+            leftsum += nums[i];
+        }
+        return -1;
+    }
+}
