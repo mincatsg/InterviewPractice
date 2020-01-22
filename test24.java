d3ae5c3710be570e00e0f6a44ba2abb59c3d7a2a
mincatsg <1374723981@qq.com>
2020-01-22 14:25:33 +0800

    hoilday

---
+++ test24.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class test24 {
+
+    //给定一个整数数组，你需要寻找一个连续的子数组，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
+
+    public int findUnsortedSubarray(int[] nums) {
+        int[] snums = nums.clone();
+        Arrays.sort(snums);
+        int start = snums.length, end = 0;
+        for (int i = 0; i < snums.length; i++) {
+            if (snums[i] != nums[i]) {
+                start = Math.min(start, i);
+                end = Math.max(end, i);
+            }
+        }
+        return (end - start >= 0 ? end - start + 1 : 0);
+        
+    }
+}
